47b5a16fe2eceae1ac39dc435c3b67fdac2d8aa5
khanhifza <Hifza6907@gmail.com>
2024-12-24 20:11:58 +0530

    Add files via upload

--- myapp/10_while.py
+++ myapp/10_while.py
@@ -1,10 +1,14 @@
 print('enter a number  to check if it EVEN or not')
-
-while True:  
-    num=int(input('enter a no - '))
-    if num % 2==0:
-     print('yes,no. is EVEN')
+user_input=""
+while user_input!= 'q':  
+    user_input=int(input('enter a no. and q for quit - '))
+    if user_input.isdigit():
+      if user_input % 2==0:
+        print('yes,no. is EVEN')
     else:
       print('No. is ODD') 
+     
+        
+    
         
    
