bfc857f4f36bea7c22ff413ac1502771fd36f41d
khanhifza <Hifza6907@gmail.com>
2024-12-25 20:52:43 +0530

    Add files via upload

--- myapp/10_while.py
+++ myapp/10_while.py
@@ -1,14 +1,49 @@
-print('enter a number  to check if it EVEN or not')
-user_input=""
-while user_input!= 'q':  
-    user_input=int(input('enter a no. and q for quit - '))
-    if user_input.isdigit():
-      if user_input % 2==0:
-        print('yes,no. is EVEN')
-    else:
-      print('No. is ODD') 
+# print('enter a number  to check if it EVEN or not')
+# user_input=""
+# while user_input!= 'q':  
+#     user_input=int(input('enter a no. and q for quit - '))
+#     if user_input.isdigit():
+#       if user_input % 2==0:
+#         print('yes,no. is EVEN')
+#     else:
+#       print('No. is ODD') 
+      
+      
+# try:
+#     # Attempt to convert the input to an integer
+#     number = int(user_input)
+#     print(f"You entered the number: {number}")
+# except ValueError:
+#     print("Error: Unable to convert input to a number.")
+
+# count=1
+
+# while count <=5:
+#   print (count)
+#   count += 1
+  
+#   msg=''
+  
+#   while msg !="quit":
+#     msg=input("your msg...")
+# print(msg)  
      
-        
+# active=True
+
+# while active:
+#   msg =input("your msg..")
+#   if msg =="exit":
+#     active=False
+    
+    # if want to remove a number or duplicacy from list
+    
+num = [21,3,21,4,67,21,6,7]
+    
+while 21 in num:
+      num.remove(21)
+      print(num)
+      
+      
     
         
    
---
+++ myapp/11_break_continue.py
@@ -0,0 +1,24 @@
+# num=[10,-20,30,-40,50,-60,70]
+
+# for i in num:
+#     if i==30:
+#         break
+#     print(i)
+
+# num=[10,-20,30,-40,50,-60,70]
+
+# for i in num:
+#     if i==30:
+#         continue
+#     print(i)
+
+num=[10,-20,30,-40,50,-60,70]
+
+for i in num:
+    if i==30:
+        break
+    else:
+     print(i)
+
+#!/bin/bash
+
